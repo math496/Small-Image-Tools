c49b9152645037c0e6c899a01fa42f59b93c7be0
Efron <Efron_Licht@yahoo.com>
2016-03-10 15:30:47 -0800

    inital commit

---
+++ grow_image.m
@@ -0,0 +1,59 @@
+function imgOut = grow_image(imgIn, varargin)
+% resample an image to be an integer proportion larger. by default,
+% grows the image by a proportion of 2 (example: 
+% grow_image(testImg): 1080x1920 --> 2060x3840.
+% varargin is an optional parameter that is a (higher) integer; eg,
+% grow_image(testImg, 5): 1080x1920 --> 5400x9600
+% this does not do any kind of nearest_neighbor smoothing or anything, it's
+% very bare-bones.
+imgIn = import_image(imgIn);
+if nargin >= 2
+    
+    if nargin > 2
+        warning('shrink_image should have at most 2 inputs: you have %g', ...
+            nargin);
+        warning('ignoring further arguments')
+    end
+    
+    try scaling_factor = varargin{1};
+        assert(isnumeric(scaling_factor));
+        assert(isscalar(scaling_factor));
+        assert(varargin{1}>1)
+        
+        if (scaling_factor-floor(scaling_factor)) ~= 0
+            warning('scaling factor %g is not an integer, treating as %g\n', ...
+                scaling_factor, floor(scaling_factor));
+        end
+        
+        scaling_factor = floor(scaling_factor);
+        
+    catch ME
+        error('second argument must be a postiive integer');
+    end
+    
+else
+    
+    scaling_factor = 2;
+
+end
+
+[rows, cols, RGB_OR_GRAY] = size(imgIn);
+
+rows = rows*scaling_factor;
+cols = cols*scaling_factor;
+
+imgOut = zeros(rows, cols, RGB_OR_GRAY, 'uint8'); %preallocate for speed
+y_out = (1:scaling_factor:rows)-1;
+x_out = (1:scaling_factor:cols)-1;
+%RESAMPLE
+for k=1:scaling_factor
+    y_out = y_out+1;
+
+    for j=1:scaling_factor
+    
+        x_out = x_out+1;
+        imgOut(y_out, x_out, :) = imgIn;
+    end    
+end
+
+end
---
+++ shrink_image.m
@@ -0,0 +1,47 @@
+function imgOut = shrink_image(imgIn, varargin)
+% resample an image to be an integer proportion smaller. by default,
+% shrinks the image by a proportion of 2 (example: 
+% shrink_image(testImg): 1080x1920 --> 540x960).
+% varargin is an optional parameter that is a (higher) integer; eg,
+% shrink_image(testImg, 5): 1080x1920 --> 216x384
+
+%% INPUT HANDLING %%
+imgIn = import_image(imgIn);
+if nargin >= 2
+    if nargin > 2
+        warning('shrink_image should have at most 2 inputs: you have %g', ...
+            nargin);
+        warning('ignoring further arguments')
+    end
+    try scaling_factor = varargin{1};
+        assert(isnumeric(scaling_factor));
+        assert(isscalar(scaling_factor));
+        assert(varargin{1}>1)
+        if (scaling_factor-floor(scaling_factor)) ~= 0
+            warning('scaling factor %g is not an integer, treating as %g\n', ...
+                scaling_factor, floor(scaling_factor));
+        end
+        scaling_factor = floor(scaling_factor);
+    catch ME
+    error(['second argument must be a postiive integer, \n']);
+    end
+else
+    scaling_factor = 2;
+end
+
+%% Trim off Bottom-Right pixels if necessary
+[rows, cols, ~] = (size(imgIn));
+if any(mod([rows, cols], scaling_factor))
+    warning('size of image not divided evenly by %g; trimming to fit', ...
+        scaling_factor);
+    rows = rows - mod(rows, scaling_factor);
+    cols = cols - mod(cols, scaling_factor);
+end 
+
+%% Resampling
+y = (1:scaling_factor:rows);
+x = (1:scaling_factor:cols);
+
+imgOut = imgIn(y, :, :);
+imgOut = imgOut(:, x, :)
+end;
